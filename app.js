7606f1d3057fba9f3beefbed4c1badbbf39b20cd
Daniel Fromer <daniel.fromer1@gmail.com>
2025-07-15 23:20:39 +0300

    doing the GET exe

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import supabase from './db.js';
 import { authenticate } from './utils.js';
 import { config } from "dotenv";
 config();
@@ -24,6 +25,37 @@ app.post('/login',async (req,res)=>{
         res.status(401).json({msg:"wrong username or password "})
     }
 })
+
+
+app.get('/products',async (req,res)=>{
+    const username = req.headers['username']
+    const password = req.headers['password']
+
+
+    if (!username || !password) {
+        return res.status(400).json({ msg: "Missing credentials in headers" });
+    }
+
+    const user = await authenticate(username, password);
+    if (!user) {
+        return res.status(401).json({ msg: "Unauthorized" });
+    }
+
+
+    const { data: products, error } = await supabase
+        .from('products')
+        .select('*');
+
+    if (error) {
+        return res.status(500).json({ msg: "Failed to fetch products", error });
+    }
+
+    res.json({ products });
+});
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`running on port ${PORT}`)
